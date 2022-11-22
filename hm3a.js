abba21730c3534b16ffd163003f436e4e7e8bffb
Krzysztof Kaczmarek <1krzysztof.kaczmarek@gmail.com>
2022-11-22 12:10:38 +0100

    next portion of exercises

--- hm3a.js
+++ hm3a.js
@@ -42,13 +42,13 @@ const financialData = require("./financial.json")
 //#####Spendings per day of the week
 //1-Monday,2-Tuesday,3-Wednesday,4-Thursday,5-Friday,6-Saturday,7-Sunday
 
-// console.log("Financial data: " + getFiancialObject().spendingPerDayofTheWeek(1))//Financial data: 5552.29
-// console.log("Financial data: " + getFiancialObject().spendingPerDayofTheWeek(2))//Financial data: 9674.619999999999
-// console.log("Financial data: " + getFiancialObject().spendingPerDayofTheWeek(3))//Financial data: 9211.930000000002
-// console.log("Financial data: " + getFiancialObject().spendingPerDayofTheWeek(4))//Financial data: 12557.249999999998
-// console.log("Financial data: " + getFiancialObject().spendingPerDayofTheWeek(5))//Financial data: 5851.7
-// console.log("Financial data: " + getFiancialObject().spendingPerDayofTheWeek(6))//Financial data: 2228.29
-// console.log("Financial data: " + getFiancialObject().spendingPerDayofTheWeek(7))//Financial data: 4511.29
+// console.log("Financial data: " + getFiancialObject().spendingPerDayofTheWeek(1)) //Financial data: 5552.29
+// console.log("Financial data: " + getFiancialObject().spendingPerDayofTheWeek(2)) //Financial data: 9674.619999999999
+// console.log("Financial data: " + getFiancialObject().spendingPerDayofTheWeek(3)) //Financial data: 9211.930000000002
+// console.log("Financial data: " + getFiancialObject().spendingPerDayofTheWeek(4)) //Financial data: 12557.249999999998
+// console.log("Financial data: " + getFiancialObject().spendingPerDayofTheWeek(5)) //Financial data: 5851.7
+// console.log("Financial data: " + getFiancialObject().spendingPerDayofTheWeek(6)) //Financial data: 2228.29
+// console.log("Financial data: " + getFiancialObject().spendingPerDayofTheWeek(7)) //Financial data: 4511.29
 
 function getFiancialObject() {
 	const financialObject = {
@@ -122,5 +122,105 @@ function getFiancialObject() {
 	return financialObject
 }
 
-
 // TODO (util functions)
+//###############################################################################
+
+const films = require("./sw-films.json")
+const planets = require("./sw-planets.json")
+const people = require("./sw-people.json")
+const starships = require("./sw-starships.json")
+
+//count sum of all starships cost from episodes 4-6
+
+function sumAllStarshipsCostFromEpisodes(startEp, endEp) {
+	let sum = 0
+	const strashipsFromEpisode46 = films
+		.filter(filterepisodes46)
+		.map(mapStarShips)
+		.flat()
+	const filteredStarShips = starships.filter(
+		filterShipsByURL(strashipsFromEpisode46)
+	)
+	return filteredStarShips.reduce(reduceByCost, 0)
+}
+
+function filterepisodes46(startEp, endEp) {
+	return epi => epi.episode_id >= startEp && epi.episode_id <= endEp
+}
+function mapStarShips(obj) {
+	return obj.starships
+}
+function filterShipsByURL(urlArray) {
+	return ship =>
+		urlArray.includes(ship.url) && ship.cost_in_credits !== "unknown"
+}
+function reduceByCost(sum, starship) {
+	sum += +starship.cost_in_credits
+	return sum
+}
+
+// console.log(
+// 	"Sum of all starships cost from episodes 4 - 6 is: " +
+// 		sumAllStarshipsCostFromEpisodes(4, 6)
+// )
+//#################################################################
+
+// find the fastest starship you can afford having 8500000 credits
+
+console.log(
+	"Fastest ship I can get for up to 8500000 is: " +
+		getFastestShipFor(8500000).name
+)
+
+function getFastestShipFor(money) {
+	let filteredship = starships
+		.filter(
+			ship =>
+				+ship.cost_in_credits <= money &&
+				ship.cost_in_credits !== "unknown" &&
+				!isNaN(ship.max_atmosphering_speed.replace("km", ""))
+		)
+		.sort((a, b) => +b.max_atmosphering_speed - +a.max_atmosphering_speed)
+
+	return filteredship[0]
+}
+
+//###################################################################
+// // find planet name with the lowest difference between the rotation period and orbital period
+
+// console.log(
+//   'Planet name with the lowest difference between the rotation period and orbital period is: ' +
+//     getPlanetNameWithLowestDifference('rotation_period', 'orbital_period')
+// );
+
+// function getPlanetNameWithLowestDifference(key1, key2) {
+//   let planetName;
+//   // TODO
+//   return planetName;
+// }
+
+// // map all starships with crew <= 4 that were created between 10 dec 2014 and 15 dec 2014
+
+// console.log(
+//   'Ships with max crew of 4 created between 10.12.2014 - 12.12.2014 number is: ' +
+//     getCrewShipFrom(4, new Date(2014, 12, 10), new Date(2014, 12, 12)).length
+// );
+
+// function getCrewShipFrom(maxCrew, dateStart, dateEnd) {
+//   let ship;
+//   // TODO
+//   return ship;
+// }
+
+// // create an array of people’s names from episodes 1 and 5 sorted by the diameter of origin planet low to high
+
+// console.log(
+//   'People from ep 1 - 5 sorted by origin planet diameter low to high: ' +
+//     getPeopleSortedByOriginPlanetDiameter(1, 5)
+// );
+
+// function getPeopleSortedByOriginPlanetDiameter(startEp, endEp) {
+//   let people;
+
+//   return people;
+// }
